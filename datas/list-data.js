3726ed686be22c71f4e594796508f5fafe80775a
crane <bhad5683126@163.com>
2018-05-24 18:48:22 +0800

    动态展示

--- datas/list-data.js
+++ datas/list-data.js
@@ -4,7 +4,8 @@ let list_data = [
     title: '尚硅谷',
     detail_img: '/images/detail/carousel/01.jpg',
     avatar: '/images/avatar/4.png',
-    detail_content: '硅谷IT教育隶属于北京晟程华科教育科技有限公司，是国内领先的专业IT教育培训机构，拥有北京、深圳两处基地。自2013年成立以来，凭借领先的教育理念、前沿的课程体系、优秀的教学团队、科学的考评制度、严格的教务管理、完备的就业保障，已经为行业输送了万余名高端技术人才。',
+    detail_content: '西蒙·海耶（Simo Häyhä，1905年12月17日—2002年4月1日），芬兰军人、著名狙击手，世界狙击之王，绰号“白色死神”（White Death）。\n' +
+    '1939年—1940年的苏芬战争中，他在不到四个月的时间里，就用芬兰版的莫辛纳甘步枪射杀了542名苏军（生涯总狙杀705人），一举成为世界上杀敌最多的狙击手。',
     reading: '112',
     collection: '96',
     headImgSrc: '/images/detail/carousel/01.jpg',
@@ -73,7 +74,7 @@ let list_data = [
   {
     date: '2018/3/15 下午 4:30:35',
     title: '贾静雯简介',
-    detail_img: '/images/detail/list/j2.jpg',
+    detail_img: '/images/detail/carousel/04.jpg',
     detail_content: '贾静雯（Alyssa Chia），1974年10月7日出生于台湾省台北市，祖籍天津市，华语影视女演员、节目主持人。1990年，贾静雯因接拍爱之味妞妞甜八宝广告而出道演艺圈；同年，她还出演了个人的首部电视剧《佳家福》。1994年，贾静雯开始担任一系列儿童节目的主持人。1997年，她凭借家庭剧《四千金》获得台湾电视金钟奖最佳新人奖[1]  。2000年，贾静雯主演的古装剧《飞龙在天》获得了台湾电视剧年度收视冠军。',
     detail_love_image1: '/images/icon/chat.png',
     detail_love_image2: '/images/icon/view.png',
@@ -93,7 +94,7 @@ let list_data = [
   {
     date: '2018/3/17 下午3:30:35',
     title: '贾静雯作品展',
-    detail_img: '/images/detail/list/j3.jpg',
+    detail_img: '/images/detail/carousel/01.jpg',
     detail_content: '2001年，贾静雯将工作重心转向内地，并主演了传奇剧《大汉天子》。2002年，她凭借武侠剧《倚天屠龙记》在内地赢得更高关注度[2]  。2003年，贾静雯获得FHM全球百大性感美女亚洲区冠军。2006年，他主演了现代剧《悲伤时唱首歌》。此后几年，贾静雯相继出演了传奇剧《太平公主秘史》、剧情片《不能说的夏天》等影视作品',
     detail_love_image1: '/images/icon/chat.png',
     detail_love_image2: '/images/icon/view.png',
@@ -114,7 +115,7 @@ let list_data = [
 
     date: 'sep 19 2016',
     title: '娱乐新闻',
-    detail_img: '/images/detail/list/j4.jpg',
+    detail_img: '/images/detail/carousel/02.jpg',
     avatar: '/images/avatar/3.png',
     detail_content: '2014年，贾静雯与小自己九岁的台湾演员修杰楷相恋[41]  。2015年5月5日，贾静雯的男友修杰楷在微博宣布了贾静雯已身怀有孕的消息，并发出了他与贾静雯和梧桐妹的两张合照，随后，贾静雯转发了此条微博并希望得到大家的祝福[42]  ；同年8月7日，贾静雯以剖宫产方式生下了与修杰楷的第一个女儿“咘咘”，而此前两人已正式注册结婚[43]  。',
     reading: '112',
@@ -138,7 +139,7 @@ let list_data = [
 
     date: 'sep 19 2016',
     title: '社会活动',
-    detail_img: '/images/detail/list/j6.jpg',
+    detail_img: '/images/detail/carousel/03.jpg',
     avatar: '/images/avatar/4.png',
     detail_content: '2012年，贾静雯担任公益大使，发起为弱势儿童和青少年课后照护计划，并为此次活动献唱了个人单曲《许一个愿望》。2014年，贾静雯参加了最终梦想年度时尚魅力女性颁奖盛典，并在典礼上获得了亲情天使奖[50] ',
     reading: '112',
--- pages/list/list.js
+++ pages/list/list.js
@@ -1,6 +1,5 @@
 // pages/list/list.js
 let datas = require('../../datas/list-data')
-console.log(datas)
 Page({
   /**
    * 组件的初始数据
@@ -8,6 +7,9 @@ Page({
   data: {
     listArr: []
   },
+  /**
+   * 生命周期函数--监听页面加载
+   */
   onLoad: function (options) {
     this.setData({
       listArr: datas.list_data
